1436d25f6f3be5c071f1e5189b7c3a96eaac2d85
Arata <arata04962@gmail.com>
2019-09-18 18:57:27 +0900

    [add] Proxy configuration

---
+++ AutoProxy/Config/AutoProxyConfig.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace AutoProxy.Config
+{
+    public class AutoProxyConfig
+    {
+        private List<ProxySetting> settings = new List<ProxySetting>();
+
+        public static AutoProxyConfig Instance { get; } = new AutoProxyConfig();
+
+        private AutoProxyConfig()
+        {
+
+        }
+
+        public void AddSetting(ProxySetting setting)
+        {
+            settings.Add(setting);
+        }
+
+        public List<ProxySetting> FindSetting(string SSIDToFind)
+        {
+            return settings.FindAll(delegate (ProxySetting s)
+            {
+                return s.SSID == SSIDToFind;
+            });
+        }
+    }
+}
---
+++ AutoProxy/Config/ProxySetting.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoProxy.Config
+{
+    public class ProxySetting
+    {
+        public string SSID;
+        public string ProxyHost;
+        public int ProxyPort;
+        public string ProxyServer
+        {
+            get { return $"{ProxyHost}:{ProxyPort}"; }
+        }
+
+        public ProxySetting()
+        {
+
+        }
+
+        public ProxySetting(string ssid, string proxyHost, int proxyPort)
+        {
+            this.SSID = ssid;
+            this.ProxyHost = proxyHost;
+            this.ProxyPort = proxyPort;
+        }
+    }
+}
