b3cd0c41e4f4d50bf264fc8f1ec5eda6527a60a3
Arata <arata04962@gmail.com>
2019-09-18 19:02:51 +0900

    [add] (Network | Notify) services

---
+++ AutoProxy/Services/ProxySettingService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text;
+using Microsoft.Win32;
+using AutoProxy.Config;
+
+namespace AutoProxy.Services
+{
+    public static class ProxySettingService
+    {
+
+        private static string InternetSettingsKeyName = @"Software\Microsoft\Windows\CurrentVersion\Internet Settings";
+
+        public static bool SetProxy(ProxySetting setting)
+        {
+            var registry = Registry.CurrentUser.OpenSubKey(InternetSettingsKeyName, true);
+            if (registry == null)
+            {
+                return false;   
+            }
+            registry.SetValue("ProxyEnable", 1);
+            registry.SetValue("ProxyServer", setting.ProxyServer);
+            return true;
+        }
+
+        public static bool UnsetProxy()
+        {
+            var registry = Registry.CurrentUser.OpenSubKey(InternetSettingsKeyName, true);
+            if (registry == null)
+            {
+                return false;
+            }
+            registry.SetValue("ProxyEnable", 0);
+            registry.SetValue("ProxyServer", "");
+            return true;
+        }
+    }
+}
